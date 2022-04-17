e8a6e402dac35e44ad7c44195e43f34b936ff818
Vasya-556 <91149467+Vasya-556@users.noreply.github.com>
2022-04-17 18:17:08 +0300

    Add files via upload

---
+++ labs-8.1.1.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str)
+{
+	if (strlen(str) < 3)
+		return 0;
+
+	int j = 0;
+	int l = 0;
+	int k = 0;
+	for (int i = 1; str[i + 1] != 0; i++) {
+		l++;
+		if (str[i] == ',') {
+			k++;
+			if (k == 3)
+				j = l;
+		}
+	}
+	return j;
+}
+
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "third coma: " << Count(str) << endl;
+	return 0;
+}
